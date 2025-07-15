7d9af8e1106200f49a76deeb2bc8cacf82f5dea4
yuuiti <youyilang69@gmail.com>
2025-07-15 14:18:17 +0900

    Changed the HTML for the site.

--- mydg/diary/urls.py
+++ mydg/diary/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("page/<uuid:id>/", views.page_detail, name="page_detail"),
     path("page/<uuid:id>/update/", views.page_update, name="page_update"),
     path("page/<uuid:id>/delete/",views.page_delete, name="page_delete"),
+    #path('diary/', views.p, name='diary'),  # '/diary/' に対応するビュー
 ]
