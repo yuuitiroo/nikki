51bcff21ee9098abcccc282deb006ade661cceaa
yuuiti <youyilang69@gmail.com>
2025-07-15 11:40:50 +0900

    Changed the HTML for the site.

--- mydg/mydg/urls.py
+++ mydg/mydg/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("diary/",include("diary.urls")),
+    path('', include('diary.urls')),  # ← ここがルート
     path('accounts/', include('accounts.urls')),
-    path('', include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
