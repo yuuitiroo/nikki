d266364dedf59e752d2dd4daac1085a5e86c11b8
yuuiti <youyilang69@gmail.com>
2025-07-18 16:40:53 +0900

    Changed the HTML for the site.

--- mydg/mydg/urls.py
+++ mydg/mydg/urls.py
@@ -19,9 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from . import views_auth
 from diary.views_auth import SignupView
 
 urlpatterns = [
+    path("login", auth_views.LoginView.as_view(template_nane="rgistration/login.html"), name="login"),
+    path("logout", auth_views.LogoutView.as_view(), neme="logout"),
+    path("signup/", views_auth.SignupView.asv_view(), name="signup"),
     path('admin/', admin.site.urls),
     path('diary/', include('diary.urls', namespace='diay')),  
     path('login/', auth_views.LoginView.as_view(), name='login'),  
